55b8f4eb2b28fea24966eca4c79ff64b33887fd4
Samuel <201501502@rajalakshmi.edu.in>
2023-02-10 11:37:17 +0530

    All Major Updates Released

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 app.config['icons'] = images
 fav_icon = os.path.join(app.config['icons'], 'logo.png')
+load_img = os.path.join(app.config['icons'], 'printease.gif')
 
 def connect_db():
     connection = sqlite3.connect('users.db')
@@ -17,7 +18,7 @@ def connect_db():
 def index():
     if 'username' in session:
         return redirect(url_for('dashboard'))
-    return render_template("index.html", fav_icon=fav_icon)
+    return render_template("index.html", fav_icon=fav_icon, load_img=load_img)
    
 
 @app.route('/signup', methods=['GET', 'POST'])
@@ -38,7 +39,7 @@ def signup():
         connection.commit()
         connection.close()
         return redirect('/login')
-    return render_template('index.html', fav_icon=fav_icon)
+    return render_template('index.html', fav_icon=fav_icon, load_img=load_img)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -55,7 +56,7 @@ def login():
             session['username'] = useremail
             return redirect('/dashboard')
         return "Invalid username or password"
-    return render_template('index.html', fav_icon=fav_icon)
+    return render_template('index.html', fav_icon=fav_icon, load_img=load_img)
 
 @app.route('/dashboard',methods=['GET', 'POST'])
 def dashboard():
@@ -70,7 +71,7 @@ def dashboard():
     conn.close()
 
     # Render the dashboard template with the username and message
-    return render_template('dashboard.html', username=username, fav_icon=fav_icon)
+    return render_template('dashboard.html', username=username, fav_icon=fav_icon, load_img=load_img)
 
 @app.route("/upload-file", methods=["POST"])
 def upload_file():
