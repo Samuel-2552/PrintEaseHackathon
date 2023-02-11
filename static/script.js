7d9b5e4fb7ce36d22c9d3bf985f575068a61283d
Shivesh16 <92038870+Shivesh16@users.noreply.github.com>
2023-02-11 21:09:49 +0530

    UPDATED SUBMIT BUTTON

--- static/script.js
+++ static/script.js
@@ -120,4 +120,13 @@ $( document ).ready(function() {
 //     document.getElementById("ques3").hidden=true;
     
 // }
-  
+var wrapper = $( "#button-wrapper" );
+
+$( ".submit" ).click(function() {
+      if(wrapper.not( ".checked" )) {
+            wrapper.addClass( "checked" );
+            setTimeout(function(){
+                wrapper.removeClass( "checked" );
+            }, 8000);
+       }
+});
