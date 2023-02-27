740aa051124119835a203f90ca4d2742bf9f7482
Samuel <201501502@rajalakshmi.edu.in>
2023-02-27 17:46:54 +0530

    code for admin

--- app.py
+++ app.py
@@ -401,5 +401,37 @@ def verify():
     return render_template('verification.html',fav_icon=fav_icon, load_img=load_img,user=user,wallet=wallet_money[0])
 
 
+@app.route('/admin')
+def index():
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM user')
+    users = cursor.fetchall()
+    cursor.close()
+    return render_template('admin.html', users=users)
+
+@app.route('/edit/<int:user_id>', methods=['GET', 'POST'])
+def edit(user_id):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM user WHERE id=?', (user_id,))
+    user = cursor.fetchone()
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        wallet = request.form['wallet']
+        EV = request.form['EV']
+        cursor.execute('UPDATE user SET username=?, password=?, email=?, wallet=?, EV=? WHERE id=?', (username, password, email, wallet, EV, user_id))
+        conn.commit()
+        cursor.close()
+        return redirect('/')
+    
+    cursor.close()
+    return render_template('edit.html', user=user)
+
+
+
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0")
