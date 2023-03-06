78c65015d4ef880288c5a5273e5b169f5b19e274
Santhosh <santhoshhariharan1393@gmail.com>
2023-03-07 00:12:13 +0530

    Admin & edit page access

--- app.py
+++ app.py
@@ -589,24 +589,31 @@ def index():
 
 @app.route('/edit/<int:user_id>', methods=['GET', 'POST'])
 def edit(user_id):
-    conn = sqlite3.connect('users.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM user WHERE id=?', (user_id,))
-    user = cursor.fetchone()
+    try:
+        if session['username'] == 'printease2023@gmail.com':
+            conn = sqlite3.connect('users.db')
+            cursor = conn.cursor()
+            cursor.execute('SELECT * FROM user WHERE id=?', (user_id,))
+            user = cursor.fetchone()
+        
 
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        email = request.form['email']
-        wallet = request.form['wallet']
-        EV = request.form['EV']
-        cursor.execute('UPDATE user SET username=?, password=?, email=?, wallet=?, EV=? WHERE id=?', (username, password, email, wallet, EV, user_id))
-        conn.commit()
-        cursor.close()
-        return redirect('/')
-    
-    cursor.close()
-    return render_template('edit.html', user=user)
+            if request.method == 'POST':
+                username = request.form['username']
+                password = request.form['password']
+                email = request.form['email']
+                wallet = request.form['wallet']
+                EV = request.form['EV']
+                cursor.execute('UPDATE user SET username=?, password=?, email=?, wallet=?, EV=? WHERE id=?', (username, password, email, wallet, EV, user_id))
+                conn.commit()
+                cursor.close()
+                return redirect('/')
+            
+            cursor.close()
+            return render_template('edit.html', user=user)
+        else:
+            return "Login as Admin"
+    except:
+          return "Login as Admin"
 
 
 
