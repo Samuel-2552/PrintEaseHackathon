c0357906513a7ba7fef9ba30f9d542ec9cbfbd16
Vejaysundaram <vejaysundaramrajkumar@gmail.com>
2023-03-20 19:19:25 +0530

    base for new product updation

--- app.py
+++ app.py
@@ -291,6 +291,41 @@ def shopdashboard():
     if request.method == 'POST':
         useremailf = request.form['logemail']
     return render_template('shopdashboard.html', fav_icon=fav_icon, load_img=load_img, ip=ip)
+# new product
+@app.route('/newproduct', methods=['GET', 'POST'])
+def newproduct():
+    if request.method == 'POST':
+        try:
+            pname = request.form['pname']
+            Qtytype = request.form['Qtytype']
+            price = request.form['price']
+            connection = connect_db()
+            cursor = connection.cursor()
+            cursor.execute("SELECT * FROM user WHERE email=?", (useremail,))
+            user = cursor.fetchone()
+            print(user)
+        except:
+            return "E-mail already Exists"
+        if user:
+            ll=1
+            # Hash the entered password and compare with the stored hash
+            password_hash = hashlib.sha256(password.encode()).hexdigest()
+            if password_hash == user[2]:
+                session['username'] = useremail
+                if useremail == 'printease2023@gmail.com':
+                    return redirect('/admin')
+                if user[5]==0:
+                    return redirect('/verification')
+                order_no+=1
+                return redirect('/dashboard')
+            else:
+                err_message="Invalid mail or password!!"
+                return render_template('index.html',mess=err_message, fav_icon=fav_icon, load_img=load_img,ip=ip,no_load=0)
+        else:
+            err_message="Invalid mail or password!!"
+            return render_template('index.html',mess=err_message, fav_icon=fav_icon, load_img=load_img,ip=ip,no_load=0)
+    return render_template('index.html', fav_icon=fav_icon, load_img=load_img,ip=ip,no_load=1)
+
 
 @app.route('/forgot', methods=['GET', 'POST'])
 def forgot():
