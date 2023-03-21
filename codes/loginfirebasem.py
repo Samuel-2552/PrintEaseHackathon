bc766f9075c7dec7c50a3d73d5544aa467955787
Meena R <211501049@rajalakshmi.edu.in>
2023-03-21 17:43:16 +0530

    .

--- codes/loginfirebasem.py
+++ codes/loginfirebasem.py
@@ -1,104 +1,59 @@
+from django.shortcuts import render
 import pyrebase
-from flask import Flask, flash, redirect, render_template, request, session, abort, url_for
-
-app = Flask(__name__)       #Initialze flask constructor
-
-#Add your own details
-config = {
-  "apiKey": "PASTE_HERE",
-  "authDomain": "PASTE_HERE",
-  "databaseURL": "PASTE_HERE",
-  "storageBucket": "PASTE_HERE"
+ 
+ 
+config={
+    apiKey: "Use Your Api Key Here",
+    authDomain: "Use Your authDomain Here",
+    databaseURL: "Use Your databaseURL Here",
+    projectId: "Use Your projectId Here",
+    storageBucket: "Use Your storageBucket Here",
+    messagingSenderId: "Use Your messagingSenderId Here",
+    appId: "Use Your appId Here"
 }
-
-#initialize firebase
-firebase = pyrebase.initialize_app(config)
-auth = firebase.auth()
-db = firebase.database()
-
-#Initialze person as dictionary
-person = {"is_logged_in": False, "name": "", "email": "", "uid": ""}
-
-#Login
-@app.route("/")
-def login():
-    return render_template("login.html")
-
-#Sign up/ Register
-@app.route("/signup")
-def signup():
-    return render_template("signup.html")
-
-#Welcome page
-@app.route("/welcome")
-def welcome():
-    if person["is_logged_in"] == True:
-        return render_template("welcome.html", email = person["email"], name = person["name"])
-    else:
-        return redirect(url_for('login'))
-
-#If someone clicks on login, they are redirected to /result
-@app.route("/result", methods = ["POST", "GET"])
-def result():
-    if request.method == "POST":        #Only if data has been posted
-        result = request.form           #Get the data
-        email = result["email"]
-        password = result["pass"]
-        try:
-            #Try signing in the user with the given information
-            user = auth.sign_in_with_email_and_password(email, password)
-            #Insert the user data in the global person
-            global person
-            person["is_logged_in"] = True
-            person["email"] = user["email"]
-            person["uid"] = user["localId"]
-            #Get the name of the user
-            data = db.child("users").get()
-            person["name"] = data.val()[person["uid"]]["name"]
-            #Redirect to welcome page
-            return redirect(url_for('welcome'))
-        except:
-            #If there is any error, redirect back to login
-            return redirect(url_for('login'))
-    else:
-        if person["is_logged_in"] == True:
-            return redirect(url_for('welcome'))
-        else:
-            return redirect(url_for('login'))
-
-#If someone clicks on register, they are redirected to /register
-@app.route("/register", methods = ["POST", "GET"])
-def register():
-    if request.method == "POST":        #Only listen to POST
-        result = request.form           #Get the data submitted
-        email = result["email"]
-        password = result["pass"]
-        name = result["name"]
-        try:
-            #Try creating the user account using the provided data
-            auth.create_user_with_email_and_password(email, password)
-            #Login the user
-            user = auth.sign_in_with_email_and_password(email, password)
-            #Add data to global person
-            global person
-            person["is_logged_in"] = True
-            person["email"] = user["email"]
-            person["uid"] = user["localId"]
-            person["name"] = name
-            #Append data to the firebase realtime database
-            data = {"name": name, "email": email}
-            db.child("users").child(person["uid"]).set(data)
-            #Go to welcome page
-            return redirect(url_for('welcome'))
-        except:
-            #If there is any error, redirect to register
-            return redirect(url_for('register'))
-
-    else:
-        if person["is_logged_in"] == True:
-            return redirect(url_for('welcome'))
-        else:
-            return redirect(url_for('register'))
-
-if __name__ == "__main__":
-    app.run()
+# Initialising database,auth and firebase for further use
+firebase=pyrebase.initialize_app(config)
+authe = firebase.auth()
+database=firebase.database()
+ 
+def signIn(request):
+    return render(request,"Login.html")
+def home(request):
+    return render(request,"Home.html")
+ 
+def postsignIn(request):
+    email=request.POST.get('email')
+    pasw=request.POST.get('pass')
+    try:
+        # if there is no error then signin the user with given email and password
+        user=authe.sign_in_with_email_and_password(email,pasw)
+    except:
+        message="Invalid Credentials!!Please ChecK your Data"
+        return render(request,"Login.html",{"message":message})
+    session_id=user['idToken']
+    request.session['uid']=str(session_id)
+    return render(request,"Home.html",{"email":email})
+ 
+def logout(request):
+    try:
+        del request.session['uid']
+    except:
+        pass
+    return render(request,"Login.html")
+ 
+def signUp(request):
+    return render(request,"Registration.html")
+ 
+def postsignUp(request):
+     email = request.POST.get('email')
+     passs = request.POST.get('pass')
+     name = request.POST.get('name')
+     try:
+        # creating a user with the given email and password
+        user=authe.create_user_with_email_and_password(email,passs)
+        uid = user['localId']
+        idtoken = request.session['uid']
+        print(uid)
+     except:
+        return render(request, "Registration.html")
+     return render(request,"Login.html")
