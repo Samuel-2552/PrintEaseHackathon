e71251aff0929218a364ee41c4784a4bda417721
Samuel <201501502@rajalakshmi.edu.in>
2023-04-07 03:03:28 +0530

    Converted to Flask

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
+
+@app.route('/CustLog')
+def CustLog():
+    return render_template('CustLog.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
