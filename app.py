3ba1e44e04911d48606d777104a8c11562bf4fbb
Vejaysundaram <vejaysundaramrajkumar@gmail.com>
2023-06-07 14:03:54 +0530

    login editing done

--- app.py
+++ app.py
@@ -35,6 +35,11 @@ def login():
 def register():
     return render_template('register.html')
 
+@app.route('/registerbusiness')
+def registerbusiness():
+    return render_template('register business.html')
+
+
 @app.route('/forgot')
 def forgot():
     return render_template('forgot-password.html')
